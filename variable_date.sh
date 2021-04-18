8eb7eddc57ee88c499036b560c5b2bd8ee8b5f20
BEF <36014313+Toaster2luX@users.noreply.github.com>
2021-04-18 23:27:37 +0200

    Add files via upload

---
+++ variable_date.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Variable date YYYY-MM-DD + HH:MM:SS
+#DATE=$(date '+%Y-%m-%d--%T')
+
+#Variable date DD-MM-YYYY + HH:MM:SS
+DATE=$(date '+%d-%m-%Y_%T')
+
+# Pour tester
+echo
+echo $DATE
+echo
